e5d702742c252f4a7366e65d244efdd15be54307
Filipe Rother de Souza <filiperother@icloud.com>
2025-11-18 14:56:54 -0300

    init permission config

---
+++ apps/api/src/index.ts
@@ -0,0 +1,12 @@
+import { defineAbilityFor } from "@repo/auth"
+
+const ability = defineAbilityFor({ role: "MEMBER" })
+
+const userCanInviteSomeoneElse = ability.can("invite", "User")
+const userCanDeleteSomeoneElse = ability.can("delete", "User")
+
+const userCannotDeleteOtherUser = ability.cannot("delete", "User")
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteSomeoneElse)
+console.log(userCannotDeleteOtherUser)
